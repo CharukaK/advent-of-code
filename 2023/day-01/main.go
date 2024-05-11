fbba93fca61032f0aa4e4dc96c9523e4fc25adb5
Charuka Karunanayake <charukakarunanayake@gmail.com>
2024-05-11 16:36:09 +0530

    Add part-2 solution for day 01

--- 2023/day-01/main.go
+++ 2023/day-01/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strconv"
@@ -14,55 +15,96 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println("Value", *val)
+	fmt.Println("Value", val)
 
 }
 
-func calculateCalibrationVal() (*int, error) {
+type NumberHolder struct {
+	first string
+	last  string
+}
+
+func (nh *NumberHolder) register(s string) {
+	if len(nh.first) == 0 {
+		nh.first = s
+	}
+
+	nh.last = s
+}
+
+func (nh *NumberHolder) reset() {
+    nh.first = ""
+    nh.last = ""
+}
+
+func calculateCalibrationVal() (int, error) {
+	// f, err := os.OpenFile("mini_input.txt", os.O_RDONLY, 0644)
 	f, err := os.OpenFile("input.txt", os.O_RDONLY, 0644)
 
 	if err != nil {
-		return nil, err
+		return 0, err
 	}
 
-	fsInfo, err := f.Stat()
+	defer f.Close()
 
-	if err != nil {
-		return nil, err
-	}
-
-	data := make([]byte, fsInfo.Size())
-
-	if _, err = f.Read(data); err != nil {
-		return nil, err
-	}
-
+	var s = bufio.NewReader(f)
 	var val int
+	var nh = &NumberHolder{
+		first: "",
+		last:  "",
+	}
 
-	first, last := "", ""
-
-	for _, c := range data {
-		switch c {
+	for tok, err := s.ReadByte(); err == nil; tok, err = s.ReadByte() {
+		switch tok {
 		case '\n':
-			num, err := strconv.Atoi(fmt.Sprintf("%s%s", first, last))
-
+            fmt.Println(nh.first, nh.last)
+			num, err := strconv.Atoi(fmt.Sprintf("%s%s", nh.first, nh.last))
 			if err != nil {
-				return nil, err
+				return 0, nil
 			}
 
 			val += num
-			first = ""
-			last = ""
-
+            nh.reset()
 		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
-			if len(first) == 0 {
-				first = string(c)
+			nh.register(string(tok))
+		case 'o':
+			if c, err := s.Peek(2); err == nil && string(c) == "ne" {
+				nh.register("1")
 			}
-
-			last = string(c)
+		case 't':
+			if c, err := s.Peek(2); err == nil && string(c) == "wo" {
+				nh.register("2")
+			} else if c, err := s.Peek(4); err == nil && string(c) == "hree" {
+				nh.register("3")
+			}
+		case 'f':
+			if c, err := s.Peek(3); err == nil {
+				if string(c) == "ive" {
+					nh.register("5")
+				} else if string(c) == "our" {
+					nh.register("4")
+				}
+			}
+		case 's':
+			if c, err := s.Peek(2); err == nil && string(c) == "ix" {
+				nh.register("6")
+			} else if c, err := s.Peek(4); err == nil && string(c) == "even" {
+				nh.register("7")
+			}
+		case 'e':
+			if c, err := s.Peek(4); err == nil && string(c) == "ight" {
+				nh.register("8")
+			}
+		case 'n':
+			if c, err := s.Peek(3); err == nil && string(c) == "ine" {
+				nh.register("9")
+			}
+		case 'z':
+			if c, err := s.Peek(3); err == nil && string(c) == "ero" {
+				nh.register("0")
+			}
 		}
-
 	}
 
-	return &val, nil
+	return val, nil
 }
